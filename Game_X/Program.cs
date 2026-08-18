ab0966c3201c2c7669f059f1ec998b75807d5e9e
Davi_Neves <davineves418@gmail.com>
2026-08-18 20:47:02 -0300

    .

--- Game_X/Program.cs
+++ Game_X/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.Extensions.FileProviders;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
@@ -25,17 +27,43 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     app.UseHsts();
 }
+// 1. Configura para procurar index.html na RAIZ do projeto (onde o arquivo realmente está)
+app.UseDefaultFiles(new DefaultFilesOptions
+{
+    FileProvider = new PhysicalFileProvider(builder.Environment.ContentRootPath),
+    RequestPath = ""
+});
 
-app.UseStaticFiles();
+// 2. Libera o index.html na RAIZ
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(builder.Environment.ContentRootPath),
+    RequestPath = ""
+});
 
+// 3. Libera a pasta /cs externa
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(
+        Path.Combine(builder.Environment.ContentRootPath, "cs")),
+    RequestPath = "/cs"
+});
+
+// 4. Libera a pasta /js externa
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(
+        Path.Combine(builder.Environment.ContentRootPath, "js")),
+    RequestPath = "/js"
+});
 app.UseRouting();
 
 app.UseSession();
 
 app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+//app.MapControllerRoute(
+  //  name: "default",
+  //  pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
